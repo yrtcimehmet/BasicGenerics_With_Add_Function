159076f5c7dd3c7994283f0621a3c7b797f54f0c
Mehmet Yirtici <yrtci.mehmet@gmail.com>
2022-10-12 22:01:51 +0300

    Add project files.

---
+++ BasicGenerics Add Function/MyList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicGenerics_Add_Function_
+{
+    class MyList<T>
+    {
+        T[] _array;
+        T[] _tempArray;
+
+        public MyList()
+        {
+            _array = new T[0];
+        }
+        public void Add (T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+
+            _array[_array.Length - 1] = item;
+        }
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+
+    }
+    
+}
---
+++ BasicGenerics Add Function/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BasicGenerics_Add_Function_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> cities = new MyList<string>();
+            cities.Add("London");
+            cities.Add("New York");
+            cities.Add("Seattle");
+
+            Console.WriteLine(cities.Count);
+        }
+    }
+}
